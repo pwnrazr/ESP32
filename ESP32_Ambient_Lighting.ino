345f739e3ffbdf8454ee3166320a412140507daa
Amir Azmi <amirz98@hotmail.com>
2020-01-15 00:29:31 +0800

    Disabled remaining MQTT retain on publish

--- ESP32_Ambient_Lighting.ino
+++ ESP32_Ambient_Lighting.ino
@@ -161,13 +161,13 @@ void loop()
       if (doorState == LOW) 
       {
         Serial.println("DoorState LOW");
-        mqttClient.publish("esp32/doorState", 0, true, "0");
+        mqttClient.publish("esp32/doorState", 0, false, "0");
         //beep = 1;
       }
       else 
       {
         Serial.println("DoorState HIGH");
-        mqttClient.publish("esp32/doorState", 0, true, "1");
+        mqttClient.publish("esp32/doorState", 0, false, "1");
         //beep = 2;
       }
   }
