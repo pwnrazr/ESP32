c585def1bb5bcd996dea72def29ac5f7cefd745d
Amir Azmi <amirz98@hotmail.com>
2020-01-21 05:33:59 +0800

    Publish IP on MQTT connect

--- comms.h
+++ comms.h
@@ -64,6 +64,19 @@ void onMqttConnect(bool sessionPresent) {
   mqttClient.subscribe("esp32/beepFreq", 2);
   mqttClient.subscribe("esp32/reboot", 2);
   mqttClient.subscribe("esp32/reqstat", 2);
+
+  char CUR_IP[20];
+    snprintf(
+      CUR_IP,
+      20, 
+      "%d.%d.%d.%d", 
+      WiFi.localIP()[0], 
+      WiFi.localIP()[1],
+      WiFi.localIP()[2], 
+      WiFi.localIP()[3]
+    );  // convert string to char array
+    
+  mqttClient.publish("esp32/connect", 0, false, CUR_IP);  // Publish IP on connect
   
   if(haveRun == false)  // Run only once
   {
