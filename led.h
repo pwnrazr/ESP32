0bb29269fa676d17b3bc3dce9eb43c8f0e654215
Amir <1644943+pwnrazr@users.noreply.github.com>
2021-08-11 10:49:48 +0800

    Remove ledEffects

--- led.h
+++ led.h
@@ -34,8 +34,6 @@ static TaskHandle_t userTaskHandle = 0;
 
 bool ledState = true;  // LED on or off state
 
-unsigned int curEffect = 0;
-
 /** show() for ESP32
  *  Call this function instead of FastLED.show(). It signals core 0 to issue a show, 
  *  then waits for a notification that it is done.
@@ -96,100 +94,12 @@ void ledsetup() {
     }
     FastLEDshowESP32();
 }
-
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-void rainbow() 
-{
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-}
-
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
-}
-
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
-}
-
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-}
   
 void ledloop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  //gPatterns[gCurrentPatternNumber]();
-  switch(curEffect)
-  {
-    case 0:
-    rainbow();
-      break;
-      
-    case 1:
-    confetti();
-      break;
-      
-    case 2:
-    sinelon();
-      break;
-      
-    case 3:
-    bpm();
-      break;
-      
-    case 4:
-    juggle();
-      break;
-  }
-  
-  // send the 'leds' array out to the actual LED strip
-  FastLEDshowESP32();
-  // FastLED.show();
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
+  // Placeholder 
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  //EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
   //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
-}
-
-void changeledEffect()
-{
-  if(curEffect != 4)
-  {
-    curEffect++;
-  }
-  else
-  {
-    curEffect = 0;
-  }
 }
