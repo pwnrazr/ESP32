19b60d61e609da0a4ffe5f56fb6f7f38b1493a33
Amir <1644943+pwnrazr@users.noreply.github.com>
2021-08-19 16:48:28 +0800

    Guide on how to setup settings.h

--- ESP32_Ambient_Lighting.ino
+++ ESP32_Ambient_Lighting.ino
@@ -1,3 +1,15 @@
+/* To setup credentials create a settings.h with:
+  #define WIFI_SSID "SSID"
+  #define WIFI_PASSWORD "WiFi Password"
+  
+  #define OTA_PASS "OTA Password"
+  
+  #define MQTT_HOST IPAddress(192, 168, 1, 1)
+  #define MQTT_PORT 1883
+  #define MQTT_USER "MQTT user"
+  #define MQTT_PASS "MQTT pass"
+*/
+
 #include "led.h"
 #include "comms.h"
 #include "ota.h"
