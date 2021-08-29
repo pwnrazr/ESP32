835dbf222d47485d622d8297be0e2540cdeab72d
Amir <1644943+pwnrazr@users.noreply.github.com>
2021-08-30 04:46:35 +0800

    Rework sensors
    Sensors now send data at its "optimal" speed
    - AHT10 = Every 8 seconds
    - SGP30 = Every 1 second
    - Dust = Every 5 seconds because I want it to
    
    Separated all the sensor code

--- sensors.h
+++ sensors.h
@@ -4,12 +4,12 @@
 Adafruit_AHTX0 aht;
 Adafruit_SGP30 sgp;
 
+bool sensorsReady = false;  // For initial warmup of sensors
+byte sensorCount = 0;
+
 //  sgp30
 int eco2, tvoc;
 uint16_t TVOC_base, eCO2_base;
-byte sgpCount = 0;
-bool sgpReady = false;   // For 15 second initial warmup
-bool baselineReady = false;
 
 // aht10
 sensors_event_t rh, temp;
@@ -22,7 +22,10 @@ union u_tag {
    byte density_byte[4];
 } u;
 
-void sendSensorData();
+void sendDustData();
+void sendAHT10Data();
+void sendSGP30Data();
+void sendSGP30Baseline();
 
 /* return absolute humidity [mg/m^3] with approximation formula
 * @param temperature [°C]
@@ -42,84 +45,124 @@ void sensorSetup()
 
   // If you have a baseline measurement from before you can assign it to start, to 'self-calibrate'
   sgp.setIAQBaseline(37856, 38117);  // Will vary for each sensor! (eCO2_baseline, TVOC_baseline)
+
+  sendAHT10Data();  // We call this on setup as SGP30 gets temperature and humidity compensation from this sensor
 }
 
 void sensorloop()
 {
-  EVERY_N_SECONDS(3)
+  EVERY_N_SECONDS(5)    // Every N seconds because I want it to
+  { // Dust
+    sendDustData();
+  }
+
+  EVERY_N_SECONDS(8)    // Every 8 seconds because datasheet of AHT10 says 8 seconds for RH tau
+  { // AHT10 Temp and Humidity
+    sendAHT10Data();
+  }
+
+  EVERY_N_SECONDS(1)    // Every 1 second because datasheet says 1HZ sampling rate is best
+  { // SGP30 eCO2 and TVOC
+    sendSGP30Data();
+  }
+  
+  EVERY_N_SECONDS(60)   // Every 60 seconds because I have no clue
   {
-    // Request dust sensor data from arduino nano
-    Wire.requestFrom(DUST_ADDR, 4);
-    if(Wire.available() == 4)
-    {
-      for(byte i = 0; i <= 4; i++)
-      {
-        u.density_byte[i] = Wire.read();
-      }
-    }
-    
-    aht.getEvent(&rh, &temp);// populate temp and humidity objects with fresh data
-
-    temperature = temp.temperature;
-    humidity = rh.relative_humidity;
-
-    sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
-    
-    sgp.IAQmeasure();
-
-    eco2 = sgp.eCO2;  // ppb
-    tvoc = sgp.TVOC;  // ppm
-
-    sgpCount += 3;  // For 3 seconds
-    if(sgpCount >= 30)
-    {
-      sgpReady = true;
-      sgpCount = 0;
-      sgp.getIAQBaseline(&eCO2_base, &TVOC_base);
-      baselineReady = true;
-    }
-    sendSensorData();
+    sendSGP30Baseline();
   }
 }
 
-void sendSensorData()
+void sendSGP30Baseline()
 {
-  if(sgpReady)
+  sgp.getIAQBaseline(&eCO2_base, &TVOC_base);
+  
+  char eco2BaselineChar[10];
+  char tvocBaselineChar[10];
+
+  itoa(eCO2_base, eco2BaselineChar, 10);
+  itoa(TVOC_base, tvocBaselineChar, 10);
+  
+  if(sensorsReady)
+  {
+    mqttClient.publish("esp32/sensor/eco2Baseline", MQTT_QOS, false, eco2BaselineChar);
+    mqttClient.publish("esp32/sensor/tvocBaseline", MQTT_QOS, false, tvocBaselineChar);
+  }
+}
+
+void sendSGP30Data()
+{
+  if(sensorsReady == false)
+  {
+    if(sensorCount <= 30) // Wait for warmup
+    {
+      sensorCount++;
+    }
+    else
+    {
+      sensorsReady = true;
+    }
+  }
+  
+  sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
+  
+  sgp.IAQmeasure();
+
+  eco2 = sgp.eCO2;  // ppb
+  tvoc = sgp.TVOC;  // ppm
+  
+  if(sensorsReady)
   {
     char eco2Char[10];
     char tvocChar[10];
-    char temperatureChar[10];
-    char humidityChar[10];
-    char dustChar[10];
-    
-    snprintf(temperatureChar, 10, "%.2f", temperature);
-    snprintf(humidityChar, 10, "%.2f", humidity);
-    snprintf(dustChar, 10, "%.2f", u.density);
     itoa(eco2, eco2Char, 10);
     itoa(tvoc, tvocChar, 10);
   
     mqttClient.publish("esp32/sensor/eco2", MQTT_QOS, false, eco2Char);
     mqttClient.publish("esp32/sensor/tvoc", MQTT_QOS, false, tvocChar);
-    mqttClient.publish("esp32/sensor/temperature", MQTT_QOS, false, temperatureChar);
-    mqttClient.publish("esp32/sensor/humidity", MQTT_QOS, false, humidityChar);
-    mqttClient.publish("esp32/sensor/dust", MQTT_QOS, false, dustChar);
-    
-    if(baselineReady)
-    {
-      char eco2BaselineChar[10];
-      char tvocBaselineChar[10];
-
-      itoa(eCO2_base, eco2BaselineChar, 10);
-      itoa(TVOC_base, tvocBaselineChar, 10);
-      
-      mqttClient.publish("esp32/sensor/eco2Baseline", MQTT_QOS, false, eco2BaselineChar);
-      mqttClient.publish("esp32/sensor/tvocBaseline", MQTT_QOS, false, tvocBaselineChar);
-
-      baselineReady = false;
-    }
   }
   else
   {
     mqttClient.publish("esp32", MQTT_QOS, false, "NOT READY");
+  }
+}
+
+void sendAHT10Data()
+{
+  aht.getEvent(&rh, &temp); // populate temp and humidity objects with fresh data
+
+  temperature = temp.temperature;
+  humidity = rh.relative_humidity;
+
+  if(sensorsReady)
+  {
+    char temperatureChar[10];
+    char humidityChar[10];
+    
+    snprintf(temperatureChar, 10, "%.2f", temperature);
+    snprintf(humidityChar, 10, "%.2f", humidity);
+    
+    mqttClient.publish("esp32/sensor/temperature", MQTT_QOS, false, temperatureChar);
+    mqttClient.publish("esp32/sensor/humidity", MQTT_QOS, false, humidityChar);
+  }
+}
+
+void sendDustData()
+{
+  // Request dust sensor data from arduino nano
+  Wire.requestFrom(DUST_ADDR, 4);
+  if(Wire.available() == 4)
+  {
+    for(byte i = 0; i <= 4; i++)
+    {
+      u.density_byte[i] = Wire.read();
+    }
+  }
+  
+  if(sensorsReady)
+  {
+    char dustChar[10];
+    snprintf(dustChar, 10, "%.2f", u.density);
+  
+    mqttClient.publish("esp32/sensor/dust", MQTT_QOS, false, dustChar);
   }
 }
