aa9bc071af4d6895faff5f37f1836194a5f74e93
Amir <1644943+pwnrazr@users.noreply.github.com>
2021-08-31 15:09:23 +0800

    Poll AHT10 every 10 seconds instead
    Still got 0% RH in sensor data which I think is invalid?
    This might help further decrease chances of invalid data

--- sensors.h
+++ sensors.h
@@ -60,7 +60,7 @@ void sensorloop()
     sendDustData();
   }
 
-  EVERY_N_SECONDS(8)    // Every 8 seconds because datasheet of AHT10 says 8 seconds for RH tau
+  EVERY_N_SECONDS(10)    // Every 10 seconds because datasheet of AHT10 8-30 sec sample rate
   { // AHT10 Temp and Humidity
     sendAHT10Data();
   }
