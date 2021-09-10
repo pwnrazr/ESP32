3073e49715aa1694e733d61dbfff86193ed2bd1b
Amir <1644943+pwnrazr@users.noreply.github.com>
2021-09-11 01:54:14 +0800

    Remove things related to xTaskNotify which is preventing boot

--- led.h
+++ led.h
@@ -30,19 +30,12 @@ const uint8_t PROGMEM gamma8[] = {
 #define NUM_LEDS    45
 CRGB leds[NUM_LEDS];
 
-// -- The core to run FastLED.show()
-#define FASTLED_SHOW_CORE 1
-
 #define START_COLOR 8900346 // 135, 206, 250
 #define START_COLOR_R 135
 #define START_COLOR_G 206
 #define START_COLOR_B 250
 
 #define START_BRIGHTNESS 20
-
-// -- Task handles for use in the notifications
-static TaskHandle_t FastLEDshowTaskHandle = 0;
-static TaskHandle_t userTaskHandle = 0;
 
 unsigned long rgbval = START_COLOR;
 bool ledState = true;
@@ -66,45 +59,6 @@ unsigned int loopCount = 0;
 
 bool fadeColor = false;
 
-/** show() for ESP32
- *  Call this function instead of FastLED.show(). It signals core FASTLED_SHOW_CORE to issue a show, 
- *  then waits for a notification that it is done.
- */
-void FastLEDshowESP32()
-{
-  if (userTaskHandle == 0) {
-    // -- Store the handle of the current task, so that the show task can
-    //    notify it when it's done
-    userTaskHandle = xTaskGetCurrentTaskHandle();
-
-    // -- Trigger the show task
-    xTaskNotifyGive(FastLEDshowTaskHandle);
-
-    // -- Wait to be notified that it's done
-    const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 200 );
-    ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
-    userTaskHandle = 0;
-  }
-}
-
-/** show Task
- *  This function runs on core FASTLED_SHOW_CORE and just waits for requests to call FastLED.show()
- */
-void FastLEDshowTask(void *pvParameters)
-{
-  // -- Run forever...
-  for(;;) {
-    // -- Wait for the trigger
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-
-    // -- Do the show (synchronously)
-    FastLED.show();
-
-    // -- Notify the calling task
-    xTaskNotifyGive(userTaskHandle);
-  }
-}
-
 void setColor(byte inR, byte inG, byte inB)
 {
   for (int i = 0; i < NUM_LEDS; i++) 
@@ -113,23 +67,15 @@ void setColor(byte inR, byte inG, byte inB)
     leds[i].green = inG;
     leds[i].blue = inB;
   }
-  FastLEDshowESP32();
+  FastLED.show();
 }
 
 void ledsetup() {
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // LED brightness on startup
   FastLED.setBrightness(pgm_read_byte(&gamma8[map(START_BRIGHTNESS, 1, 255, 66, 255)]));
-
-  int core = xPortGetCoreID();
-  //Serial.print("Main code running on core ");
-  //Serial.println(core);
-
-  // -- Create the FastLED show task
-  xTaskCreatePinnedToCore(FastLEDshowTask, "FastLEDshowTask", 2048, NULL, 2, &FastLEDshowTaskHandle, FASTLED_SHOW_CORE);
   
   setColor(curR, curG, curB); // Sets starting led color
 }
@@ -185,7 +131,7 @@ void ledloop()
           curBrightness++;
           FastLED.setBrightness(pgm_read_byte(&gamma8[map(curBrightness, 1, 255, 66, 255)]));
         }
-        FastLEDshowESP32();
+        FastLED.show();
       }
       else
       {
